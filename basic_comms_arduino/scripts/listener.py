bea14f37fa22c1573b659f7960a551def111f260
Alex <alejandro.armidac@gmail.com>
2023-08-22 19:41:11 -0600

    New listener for extra points

--- basic_comms_arduino/scripts/listener.py
+++ basic_comms_arduino/scripts/listener.py
@@ -2,10 +2,26 @@
 import rospy
 from std_msgs.msg import String
 
+message = ""
+
 def callback(msg):
-    rospy.loginfo("I heard %s", msg.data)
+    global message
+    message = msg.data
 
 if __name__ == '__main__':
     rospy.init_node('listener')
     rospy.Subscriber("chatter", String, callback)
-    rospy.spin()
+
+    rate = rospy.Rate(100)
+
+    while not rospy.is_shutdown():
+        if int(message) > 28:
+            rospy.loginfo("Estas lejos a: " + message + " cm")
+        
+        elif int(message) < 5:
+            rospy.loginfo("Estas muy cerca a: " + message + " cm")
+
+        else:
+            rospy.loginfo("Distancia adecuada: " + message + " cm")
+
+        rate.sleep()
