3fc60a7a58f6b5e0231cb4ec57c6674587785f18
Karen-Rdz <karen.rdz.h@gmail.com>
2023-08-22 20:07:28 -0600

    Agregar aplicacion

--- basic_comms_arduino/scripts/listener.py
+++ basic_comms_arduino/scripts/listener.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 import rospy
-from std_msgs.msg import String
+from std_msgs.msg import Float32
 
-message = ""
+message = 0.0
 
 def callback(msg):
     global message
@@ -10,20 +10,20 @@ def callback(msg):
 
 if __name__ == '__main__':
     rospy.init_node('listener')
-    rospy.Subscriber("distance", String, callback)
+    rospy.Subscriber("distance", Float32, callback)
 
     rate = rospy.Rate(100)
 
     while not rospy.is_shutdown():
         # Tank filling logic. the sensor is placed facing the water to calculate its lever, when the sensor reads a height of 5 cm: its full and needs
         # to stop, on the other hand if the sensor reads more than 28 cm: the tank is empty and needs to be filled. Anything between the values is fine.
-        if int(message) > 28:
-            rospy.loginfo("El tanque esta vacio, se debe llenar. Lectura: " + message + " cm")
+        if message > 15:
+            rospy.loginfo("El tanque esta vacio, se debe llenar. Lectura: " + str(message) + " cm")
         
-        elif int(message) < 5:
-            rospy.loginfo("El tanque esta lleno, se debe parar de llenar. Lectura: " + message + " cm")
+        elif message < 5:
+            rospy.loginfo("El tanque esta lleno, se debe parar de llenar. Lectura: " + str(message) + " cm")
 
         else:
-            rospy.loginfo("El tanque esta adecuado, no hacer nada. Lectura: " + message + " cm")
+            rospy.loginfo("El tanque se está llenando. Lectura: " + str(message) + " cm")
 
         rate.sleep()
