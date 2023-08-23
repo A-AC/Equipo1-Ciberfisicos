6fd60d92017e12c5b574a6a6a722190cb3b18722
Alex <alejandro.armidac@gmail.com>
2023-08-22 19:48:10 -0600

    New water tank implementation

--- basic_comms_arduino/scripts/listener.py
+++ basic_comms_arduino/scripts/listener.py
@@ -10,18 +10,20 @@ def callback(msg):
 
 if __name__ == '__main__':
     rospy.init_node('listener')
-    rospy.Subscriber("chatter", String, callback)
+    rospy.Subscriber("distance", String, callback)
 
     rate = rospy.Rate(100)
 
     while not rospy.is_shutdown():
+        # Tank filling logic. the sensor is placed facing the water to calculate its lever, when the sensor reads a height of 5 cm: its full and needs
+        # to stop, on the other hand if the sensor reads more than 28 cm: the tank is empty and needs to be filled. Anything between the values is fine.
         if int(message) > 28:
-            rospy.loginfo("Estas lejos a: " + message + " cm")
+            rospy.loginfo("El tanque esta vacio, se debe llenar. Lectura: " + message + " cm")
         
         elif int(message) < 5:
-            rospy.loginfo("Estas muy cerca a: " + message + " cm")
+            rospy.loginfo("El tanque esta lleno, se debe parar de llenar. Lectura: " + message + " cm")
 
         else:
-            rospy.loginfo("Distancia adecuada: " + message + " cm")
+            rospy.loginfo("El tanque esta adecuado, no hacer nada. Lectura: " + message + " cm")
 
         rate.sleep()
